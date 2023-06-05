043ae27f9c5cd979819b05d177df670dc768331e
C0DE-SLAYER <fsali315@gmail.com>
2023-06-06 00:37:43 +0530

    basic project structure and home page half done

--- app.py
+++ app.py
@@ -4,7 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('base.html')
+    return render_template('home.html')
+
+@app.route('/random')
+def random():
+    return render_template('random.html')
+
+@app.route('/submit')
+def submit():
+    return render_template('submit.html')
+
+@app.route('/api')
+def api():
+    return render_template('apidocs.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
